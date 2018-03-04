315139ce6948f230b9e28b8824e66f1dd16ad5b9
Mina <mina@openroad.ca>
2018-03-03 20:32:23 -0800

    An introduction to C# and .Net

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >0)
+            {
+                int i = 0;
+                while(i<args.Length)
+                {
+                    Console.WriteLine("Hello, " + args[i]);
+                    i++;
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Please input your name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("How many hours of sleep did you get last night?");
+                int hoursOfSleep = int.Parse(Console.ReadLine());
+                Console.WriteLine("Hello, "+ name + "!");
+                if (hoursOfSleep>8)
+                {
+                    Console.WriteLine("You are well rested");
+                }
+                else
+                {
+                    Console.WriteLine("You need more sleep");
+                }
+            }
+        }
+    }
+}
---
+++ hello.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Program
+{
+	static void Main()
+	{
+		Console.WriteLine(DateTime.Now.DayOfWeek);	
+	}
+
+
+
+}
