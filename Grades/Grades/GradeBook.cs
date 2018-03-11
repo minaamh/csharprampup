b7b611eb23743d47db5630f11c8ff0cfbfbdc13b
Mina <mina@openroad.ca>
2018-03-10 18:33:20 -0800

    Classes and Objects

---
+++ Grades/Grades/GradeBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                sum += grade;
+            }
+            stats.averageGrade = sum / grades.Count;
+            stats.highestGrade = grades.Max();
+            stats.lowestGrade = grades.Min();
+            return stats;
+        }
+        List<float> grades;
+   
+    }
+}
---
+++ Grades/Grades/GradeStatistics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeStatistics
+    {
+       public float averageGrade;
+       public float highestGrade;
+       public float lowestGrade;
+    }
+}
